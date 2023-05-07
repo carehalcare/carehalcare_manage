25e0513cef03470cd72499ae18565cfb170679ad
6JiEun <mjkim12160@gmail.com>
2023-05-08 01:28:27 +0900

    간병게시판 UI

---
+++ app/src/main/java/carehalcare/carehalcare_manage/Feature_carereport/RecordActivity.java
@@ -0,0 +1,19 @@
+package carehalcare.carehalcare_manage.Feature_carereport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import carehalcare.carehalcare_manage.R;
+
+public class RecordActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writemain);
+    }
+}
