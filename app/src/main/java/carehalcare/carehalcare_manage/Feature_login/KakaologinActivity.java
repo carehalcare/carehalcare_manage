c5d5fd6e430b83e8fb78d7ad5a9d36bdbc33b060
6JiEun <mjkim12160@gmail.com>
2023-05-30 16:03:53 +0900

    reupload

---
+++ app/src/main/java/carehalcare/carehalcare_manage/Feature_login/KakaologinActivity.java
@@ -0,0 +1,8 @@
+package carehalcare.carehalcare_manage.Feature_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KakaologinActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/carehalcare/carehalcare_manage/Feature_login/SignupActivity.java
@@ -0,0 +1,30 @@
+package carehalcare.carehalcare_manage.Feature_login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import carehalcare.carehalcare_manage.R;
+
+public class SignupActivity extends AppCompatActivity{
+
+    Button btn_signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        btn_signup = (Button) findViewById(R.id.btn_signup);
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
